a59e04b73fcf9e4027bd8c350b49bde2e60801a9
abush <abush@PUNKOR-PC>
2018-01-23 22:38:05 +0200

    remoting one

--- RemotingOne/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RemotingOne
-{
-    public class Class1
-    {
-    }
-}
---
+++ RemotingOne/Hello.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RemotingOne
+{
+    [Serializable]
+    public class MySerialized
+    {
+        public MySerialized(int val)
+        {
+            a = val;
+        }
+        public void Foo()
+        {
+            Console.WriteLine("MySerialized.Foo called");
+        }
+        public int A
+        {
+            get
+            {
+                Console.WriteLine("MySerialized.A called");
+                return a;
+            }
+            set
+            {
+                a = value;
+            }
+        }
+        protected int a;
+    }
+
+    public class Hello : MarshalByRefObject
+    {
+        public Hello()
+        {
+            Console.WriteLine("Hello constructor");
+        }
+
+        public string Greeting(string name)
+        {
+            Console.WriteLine("Greeting called");
+            return "Hello, " + name;
+        }
+
+        public MySerialized GetMySerialized()
+        {
+            return new MySerialized(42);
+        }
+    }
+}
